6939be5139b14bacfa1755cd78b6f6d050adab9d
JRichm <jblackenmusic@gmail.com>
2023-06-04 14:21:29 -0600

    added component and pages folder for react app

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="https://google.com/">google</a></li>
+                    <li><a href="https://youtube.com/">youtube</a></li>
+                    <li><a href="https://minecraft.net/">minecraft</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
