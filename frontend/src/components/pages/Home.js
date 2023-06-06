46e05c6963713c4e3b358dc146ce6b15b0f06895
JRichm <jblackenmusic@gmail.com>
2023-06-06 12:59:33 -0600

    added cards

---
+++ frontend/src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../styles/App.css'
+import HeroSection from '../HeroSection.js'
+import Cards from '../Cards.js'
+
+function Home() {
+    return (
+        <>
+            <HeroSection />
+            <Cards />
+        </>
+    )
+}
+
+export default Home
